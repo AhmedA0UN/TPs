4f42d71a4b39daece541913aeadfce32eb450db8
Ahmed <ahmed.aoun@isimg.tn>
2025-10-09 11:02:23 +0100

    ajouter un code

---
+++ tpsss/eclipse-workspace/tp2/src/tp2/Multiplication.java
@@ -0,0 +1,10 @@
+package tp2;
+
+public class Multiplication {
+	public static void main (String args[]) {
+		int m=Integer.parseInt(args[0]);
+		for(int i=0;i<10;i++) {
+			System.out.println(m+"x"+i+"="+(m*i));
+		}
+	}
+}
---
+++ tpsss/eclipse-workspace/tp2/src/tp2/ProfitePerte.java
@@ -0,0 +1,17 @@
+package tp2;
+import java.util.Scanner;
+public class ProfitePerte {
+	public static void main (String args[]) {
+		int f=Integer.parseInt(args[0]);
+		int v=Integer.parseInt(args[0]);
+		if(f>v) {
+			System.out.println("perte");
+		}
+		else if (f<v){
+			System.out.println("profite");
+		}
+		else {
+			System.out.println("Null");
+		}
+	}
+}
