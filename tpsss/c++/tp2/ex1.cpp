74844d1a748eeac89d38abb8fe09809a304ce287
Ahmed <ahmed.aoun@isimg.tn>
2025-10-18 21:51:34 +0100

    ajout de nouveau code

--- tpsss/c++/tp2/ex1.cpp
+++ tpsss/c++/tp2/ex1.cpp
@@ -1,5 +1,24 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+
 int main() {
-    cout <<"hello" <<endl;
+    // Pointeur vers un entier
+    int* ptrInt = new int;     // Allocation dynamique
+    *ptrInt = 42;              // Initialisation
+
+    cout << "Valeur de l'entier : " << *ptrInt << endl;
+    cout << "Adresse mémoire de l'entier : " << ptrInt << endl;
+
+    // Pointeur vers un réel (double)
+    double* ptrDouble = new double;  // Allocation dynamique
+    *ptrDouble = 3.14159;            // Initialisation
+
+    cout << "Valeur du réel : " << *ptrDouble << endl;
+    cout << "Adresse mémoire du réel : " << ptrDouble << endl;
+
+    // Libération de la mémoire
+    delete ptrInt;
+    delete ptrDouble;
+
+    return 0;
 }
