06dd4ebd36ca5171c2b205ee5c4d3a3a06c6528b
Ahmed <ahmed.aoun@isimg.tn>
2025-11-05 06:29:33 +0100

    ajouter un document sources

--- tpsss/c++/tp2/ex1.cpp
+++ tpsss/c++/tp2/ex1.cpp
@@ -2,6 +2,15 @@
 using namespace std;
 
 int main() {
+
+    int a = 42;           
+    float b = 3.14f;      
+
+    cout << "Entier statique: " << a << ", adresse: " << &a << endl;
+    cout << "Réel statique: " << b << ", adresse: " << &b << endl;
+
+
+
     int* ptrInt = new int(42);
     cout << "Entier : " << *ptrInt << ", Adresse : " << ptrInt << endl;
 
